8e813bcb73e5d1e285c9b942a4ff1498a38077d7
daniela1 <danieludokike@gmail.com>
2021-07-19 13:51:56 +0100

    Commit for changing git repo

--- omor/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('omorapp.urls')),
-    path("account/", include("django.contrib.auth.urls")),
-]
--- omorapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OmorappConfig(AppConfig):
-    name = 'omorapp'
